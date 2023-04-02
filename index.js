38b65cf0c5edeb6905e7e598228eb4fcaef0ab82
Taylor Burks <tayyy25@gmail.com>
2023-04-01 21:00:11 -0400

    rithmtic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+{let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2}
+
+{let random = 10
+    Math.floor(Math.random() * 10) + 1;}
+
+{let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4}
+
+let max = (1, 2, 5, 10, 15, 20)
